85b11db895b26945adceeadd9debc83af7ca7425
Alissa Rao <lymia@lymiahugs.com>
2021-11-22 17:07:53 -0800

    Some final fixes to the repository.

--- kubejs/startup_scripts/fixes_worldgen.js
+++
@@ -1,5 +0,0 @@
-onEvent('worldgen.remove', e => {
-    e.removeOres(ore => { ore.blocks = [
-        'chisel:marble/raw', 'chisel:limestone/raw', 'chisel:basalt/raw', "darkerdepths:silver_ore"
-    ] })
-})
